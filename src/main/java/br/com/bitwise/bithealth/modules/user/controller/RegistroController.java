24c6ea3cee891949ea6992f4547e0a050ce65b25
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-27 19:13:17 -0300

    feat: controller de registro

--- src/main/java/br/com/bitwise/bithealth/modules/user/controller/RegistroController.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/controller/RegistroController.java
@@ -1,4 +1,35 @@
 package br.com.bitwise.bithealth.modules.user.controller;
 
+import br.com.bitwise.bithealth.modules.user.dto.RegistroUsuarioDTO;
+import br.com.bitwise.bithealth.modules.user.model.Usuario;
+import br.com.bitwise.bithealth.modules.user.service.RegistroService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class RegistroController {
+
+    @Autowired
+    private RegistroService registroService;
+
+    @PostMapping("/registro")
+    public ResponseEntity<?> registrarUsuario(@Valid @RequestBody RegistroUsuarioDTO registroDTO) {
+        try {
+            Usuario usuarioRegistrado = registroService.registrarNovoUsuario(registroDTO);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(usuarioRegistrado);
+        } catch (RuntimeException e) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(e.getMessage());
+        }
+    }
 }
