f549c777336020a071643fe5ec38e0a84e2ea5be
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-27 19:13:27 -0300

    feat: loginRequestDTO

--- src/main/java/br/com/bitwise/bithealth/modules/user/dto/LoginRequestDTO.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/dto/LoginRequestDTO.java
@@ -1,4 +1,14 @@
 package br.com.bitwise.bithealth.modules.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequestDTO {
+
+    @NotBlank(message = "Email é obrigatório")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    private String senha;
 }
