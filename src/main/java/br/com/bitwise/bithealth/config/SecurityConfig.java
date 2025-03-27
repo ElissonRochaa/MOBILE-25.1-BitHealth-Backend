25317e6972f36db3b76d5ba391a2477a212ed34a
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-27 19:13:05 -0300

    feat: configuracoes do security config

--- src/main/java/br/com/bitwise/bithealth/config/SecurityConfig.java
+++ src/main/java/br/com/bitwise/bithealth/config/SecurityConfig.java
@@ -1,14 +1,27 @@
 package br.com.bitwise.bithealth.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.Key;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private Key jwtSecretKey;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -20,6 +33,9 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
                         ).permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMINISTRADOR")
+                        .requestMatchers("/api/cidadao/**").hasRole("CIDADAO")
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form.disable())
