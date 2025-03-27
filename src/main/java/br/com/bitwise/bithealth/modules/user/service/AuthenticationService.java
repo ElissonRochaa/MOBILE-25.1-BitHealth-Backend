f1b3362b20e6d60732e2008bd3f031b25e55b4b2
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-27 19:14:17 -0300

    feat: authenticationService

--- src/main/java/br/com/bitwise/bithealth/modules/user/service/AuthenticationService.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/service/AuthenticationService.java
@@ -1,4 +1,62 @@
 package br.com.bitwise.bithealth.modules.user.service;
 
+import br.com.bitwise.bithealth.modules.user.dto.LoginRequestDTO;
+import br.com.bitwise.bithealth.modules.user.dto.LoginResponseDTO;
+import br.com.bitwise.bithealth.modules.user.model.Usuario;
+import br.com.bitwise.bithealth.modules.user.repository.UsuarioRepository;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwts;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Optional;
+
+@Service
 public class AuthenticationService {
+    @Autowired
+    private Key jwtSecretKey;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private final long EXPIRATION_TIME = 864_000_000;
+
+    public LoginResponseDTO authenticate(LoginRequestDTO loginRequest) {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(loginRequest.getEmail());
+
+        if (usuarioOptional.isPresent()) {
+            Usuario usuario = usuarioOptional.get();
+
+            if (passwordEncoder.matches(loginRequest.getSenha(), usuario.getSenha())) {
+                String token = generateJwtToken(usuario);
+
+                LoginResponseDTO response = new LoginResponseDTO();
+                response.setId(usuario.getId());
+                response.setNome(usuario.getNome());
+                response.setEmail(usuario.getEmail());
+                response.setToken(token);
+
+                return response;
+            }
+        }
+
+        throw new RuntimeException("Credenciais inválidas");
+    }
+
+    private String generateJwtToken(Usuario usuario) {
+        return Jwts.builder()
+                .setSubject(usuario.getEmail())
+                .claim("userId", usuario.getId())
+                .claim("userType", usuario.getTipoUsuario())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(jwtSecretKey)
+                .compact();
+    }
 }
