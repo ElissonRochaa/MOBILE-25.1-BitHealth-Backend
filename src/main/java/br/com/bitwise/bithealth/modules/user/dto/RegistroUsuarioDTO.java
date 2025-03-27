87fb6be6d352607fc6952f532b92a812153cc95a
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-27 19:13:38 -0300

    feat: registroUsuarioDTO

--- src/main/java/br/com/bitwise/bithealth/modules/user/dto/RegistroUsuarioDTO.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/dto/RegistroUsuarioDTO.java
@@ -1,4 +1,31 @@
 package br.com.bitwise.bithealth.modules.user.dto;
 
+import br.com.bitwise.bithealth.modules.user.model.ENUM.TipoUsuario;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
 public class RegistroUsuarioDTO {
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
+    private String nome;
+
+    @NotBlank(message = "Sobrenome é obrigatório")
+    @Size(min = 2, max = 100, message = "Sobrenome deve ter entre 2 e 100 caracteres")
+    private String sobrenome;
+
+    @NotBlank(message = "CPF é obrigatório")
+    private String cpf;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    @Size(max = 150, message = "Email não pode exceder 150 caracteres")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 8, message = "Senha deve ter no mínimo 8 caracteres")
+    private String senha;
+
+    @NotNull(message = "Tipo de usuário é obrigatório")
+    private TipoUsuario tipoUsuario;
 }
