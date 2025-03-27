e3c2040ecd2405774b15ef5829ecfd2ab02e3f33
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-27 19:14:08 -0300

    feat: usuarioRepository

--- src/main/java/br/com/bitwise/bithealth/modules/user/repository/UsuarioRepository.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/repository/UsuarioRepository.java
@@ -1,4 +1,14 @@
 package br.com.bitwise.bithealth.modules.user.repository;
 
-public class UsuarioRepository {
+import br.com.bitwise.bithealth.modules.user.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByCpf(String cpf);
+    boolean existsByEmail(String email);
+    boolean existsByCpf(String cpf);
 }
