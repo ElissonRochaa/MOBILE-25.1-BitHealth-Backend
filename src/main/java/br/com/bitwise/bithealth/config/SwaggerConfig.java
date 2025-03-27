dadd89e9a7a4733f9aa584442e004f22031e1296
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-27 17:14:46 -0300

    feat: configurando swagger

--- src/main/java/br/com/bitwise/bithealth/config/SwaggerConfig.java
+++ src/main/java/br/com/bitwise/bithealth/config/SwaggerConfig.java
@@ -1,4 +1,17 @@
 package br.com.bitwise.bithealth.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info()
+                .title("BitHealth API")
+                .description("API responsável pelo back-end do BitHealth")
+                .version("0.0.5"));
+    }
 }
