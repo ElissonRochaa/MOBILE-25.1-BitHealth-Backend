44acc3aabffb1730cec3de557cfd10478245f3ee
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-27 19:13:11 -0300

    feat: controller de autenticacao

--- src/main/java/br/com/bitwise/bithealth/modules/user/controller/AuthenticationController.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/controller/AuthenticationController.java
@@ -1,4 +1,26 @@
 package br.com.bitwise.bithealth.modules.user.controller;
 
+import br.com.bitwise.bithealth.modules.user.dto.LoginRequestDTO;
+import br.com.bitwise.bithealth.modules.user.dto.LoginResponseDTO;
+import br.com.bitwise.bithealth.modules.user.service.AuthenticationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginRequest) {
+        LoginResponseDTO response = authenticationService.authenticate(loginRequest);
+        return ResponseEntity.ok(response);
+    }
 }
