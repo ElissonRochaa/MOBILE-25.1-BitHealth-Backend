a5c4a9a6721da83f493678fc9a368c57818c4880
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-27 19:13:33 -0300

    feat: loginResponseDTO

--- src/main/java/br/com/bitwise/bithealth/modules/user/dto/LoginResponseDTO.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/dto/LoginResponseDTO.java
@@ -1,4 +1,13 @@
 package br.com.bitwise.bithealth.modules.user.dto;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class LoginResponseDTO {
+    private UUID id;
+    private String nome;
+    private String email;
+    private String token;
 }
