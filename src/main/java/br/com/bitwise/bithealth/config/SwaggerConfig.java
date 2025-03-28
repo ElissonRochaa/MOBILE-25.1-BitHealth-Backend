0f9999d637cf2c1e4bf55383dbac06c3d2e8f218
Jeova BL <jeovab115@gmail.com>
2025-03-28 16:31:06 -0300

    feat: add JWT authentication security no schema do swagger

--- src/main/java/br/com/bitwise/bithealth/config/SwaggerConfig.java
+++ src/main/java/br/com/bitwise/bithealth/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package br.com.bitwise.bithealth.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,6 +13,15 @@ public class SwaggerConfig {
         return new OpenAPI().info(new Info()
                 .title("BitHealth API")
                 .description("API responsável pelo back-end do BitHealth")
-                .version("0.0.5"));
+                .version("0.0.5"))
+                .schemaRequirement("JWTAuth", createSecurityScheme());
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme()
+                .name("JWTAuth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
     }
 }
