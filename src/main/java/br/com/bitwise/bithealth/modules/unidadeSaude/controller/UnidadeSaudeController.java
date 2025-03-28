790f1b238ea40fe246102b40e48c9ca592622e0e
Jeova BL <jeovab115@gmail.com>
2025-03-28 16:32:15 -0300

    Refactor: add JWT security nos endpoints do controller

--- src/main/java/br/com/bitwise/bithealth/modules/unidadeSaude/controller/UnidadeSaudeController.java
+++ src/main/java/br/com/bitwise/bithealth/modules/unidadeSaude/controller/UnidadeSaudeController.java
@@ -3,6 +3,7 @@ package br.com.bitwise.bithealth.modules.unidadeSaude.controller;
 import br.com.bitwise.bithealth.modules.unidadeSaude.dto.UnidadeSaudeRequest;
 import br.com.bitwise.bithealth.modules.unidadeSaude.dto.UnidadeSaudeResponse;
 import br.com.bitwise.bithealth.modules.unidadeSaude.services.UnidadeSaudeService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +22,14 @@ public class UnidadeSaudeController {
     private final UnidadeSaudeService unidadeSaudeService;
 
     @GetMapping("/")
+    @SecurityRequirement(name = "JWTAuth")
     public ResponseEntity<List<UnidadeSaudeResponse>> getAllUnidades() {
         List<UnidadeSaudeResponse> unidades = unidadeSaudeService.getAllUnidadeSaude();
         return ResponseEntity.ok().body(unidades);
     }
 
     @PostMapping("/")
+    @SecurityRequirement(name = "JWTAuth")
     public ResponseEntity<UnidadeSaudeResponse> createUnidade(@RequestBody @Valid UnidadeSaudeRequest unidadeRequest) {
         UnidadeSaudeResponse unidade = unidadeSaudeService.createUnidadeSaude(unidadeRequest);
         URI uri = URI.create("/unidades/" + unidade.tokenId());
@@ -34,6 +37,7 @@ public class UnidadeSaudeController {
     }
 
     @DeleteMapping("/{id}")
+    @SecurityRequirement(name = "JWTAuth")
     public ResponseEntity<Void> deleteUnidade(@PathVariable String tokenId) {
         unidadeSaudeService.deleteUnidadeSaude(tokenId);
         return ResponseEntity.ok().build();
