9afc8b1767351a3955ac2acefc1b51eb42dc4265
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-28 10:22:56 -0300

    refac: trocando entidade por dto

--- src/main/java/br/com/bitwise/bithealth/modules/user/service/RegistroService.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/service/RegistroService.java
@@ -1,8 +1,10 @@
 package br.com.bitwise.bithealth.modules.user.service;
 
 import br.com.bitwise.bithealth.modules.user.dto.RegistroUsuarioDTO;
+import br.com.bitwise.bithealth.modules.user.dto.UsuarioDTO;
 import br.com.bitwise.bithealth.modules.user.exceptions.CPFAlreadyExistsException;
 import br.com.bitwise.bithealth.modules.user.exceptions.EmailAlreadyExistsException;
+import br.com.bitwise.bithealth.modules.user.mapper.UsuarioMapper;
 import br.com.bitwise.bithealth.modules.user.model.Usuario;
 import br.com.bitwise.bithealth.modules.user.repository.UsuarioRepository;
 import jakarta.transaction.Transactional;
@@ -12,16 +14,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegistroService {
 
-    private UsuarioRepository usuarioRepository;
-    private PasswordEncoder passwordEncoder;
+    private final UsuarioMapper usuarioMapper;
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public RegistroService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+    public RegistroService(UsuarioRepository usuarioRepository,
+                           PasswordEncoder passwordEncoder,
+                           UsuarioMapper usuarioMapper) {
         this.usuarioRepository = usuarioRepository;
         this.passwordEncoder = passwordEncoder;
+        this.usuarioMapper = usuarioMapper;
     }
 
+
     @Transactional
-    public Usuario registrarNovoUsuario(RegistroUsuarioDTO registroDTO) {
+    public UsuarioDTO registrarNovoUsuario(RegistroUsuarioDTO registroDTO) {
         if (usuarioRepository.existsByEmail(registroDTO.email())) {
             throw new EmailAlreadyExistsException("Email já cadastrado");
         }
@@ -30,18 +37,12 @@ public class RegistroService {
             throw new CPFAlreadyExistsException("CPF já cadastrado");
         }
 
-        Usuario novoUsuario = new Usuario();
-        novoUsuario.setNome(registroDTO.nome());
-        novoUsuario.setSobrenome(registroDTO.sobrenome());
-        novoUsuario.setCpf(registroDTO.cpf());
-        novoUsuario.setEmail(registroDTO.email());
+        Usuario usuario = usuarioMapper.toEntity(registroDTO);
+        usuario.setSenha(passwordEncoder.encode(registroDTO.senha()));
+        usuario.setAtivo(true);
 
-        novoUsuario.setSenha(passwordEncoder.encode(registroDTO.senha()));
+        Usuario usuarioSalvo = usuarioRepository.save(usuario);
+        return usuarioMapper.toDto(usuarioSalvo);
 
-        novoUsuario.setTipoUsuario(registroDTO.tipoUsuario());
-
-        novoUsuario.setAtivo(true);
-
-        return usuarioRepository.save(novoUsuario);
     }
 }
