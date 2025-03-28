b7c84149a0bdf521efa679312d2c13912074537e
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-28 19:45:51 -0300

    feat: adicionando handler de cpf e email

--- src/main/java/br/com/bitwise/bithealth/modules/user/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/handler/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package br.com.bitwise.bithealth.modules.user.handler;
 
+import br.com.bitwise.bithealth.modules.user.dto.MensagemRespostaDTO;
+import br.com.bitwise.bithealth.modules.user.exceptions.CPFAlreadyExistsException;
+import br.com.bitwise.bithealth.modules.user.exceptions.EmailAlreadyExistsException;
 import br.com.bitwise.bithealth.modules.user.exceptions.UserGenerateTokenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +10,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,4 +45,20 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ex.getMessage());
     }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<MensagemRespostaDTO> handleEmailAlreadyExists(EmailAlreadyExistsException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(MensagemRespostaDTO.erro(ex.getMessage()));
+    }
+
+    @ExceptionHandler(CPFAlreadyExistsException.class)
+    public ResponseEntity<MensagemRespostaDTO> handleCpfAlreadyExists(CPFAlreadyExistsException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(MensagemRespostaDTO.erro(ex.getMessage()));
+    }
+
+
 }
