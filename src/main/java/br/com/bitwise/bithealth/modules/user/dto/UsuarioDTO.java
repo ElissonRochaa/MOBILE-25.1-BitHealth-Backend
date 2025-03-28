f686a9ad422c8096d7c3109f8adf8e2f27cd7130
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-03-28 10:21:58 -0300

    feat: UsuarioDTO

--- src/main/java/br/com/bitwise/bithealth/modules/user/dto/UsuarioDTO.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/dto/UsuarioDTO.java
@@ -1,4 +1,34 @@
 package br.com.bitwise.bithealth.modules.user.dto;
 
-public class UserDTO {
+import br.com.bitwise.bithealth.modules.user.model.ENUM.TipoUsuario;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDTO {
+    private UUID id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String cpf;
+
+    private String email;
+
+    private String senha;
+
+    private TipoUsuario tipoUsuario;
+
+    private Boolean ativo = true;
+
+    private LocalDateTime criadoEm = LocalDateTime.now();
 }
