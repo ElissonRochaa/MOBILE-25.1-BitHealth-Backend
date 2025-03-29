f625df5d09af55412d2275bb654d9749d0e6a7a7
Jeova BL <jeovab115@gmail.com>
2025-03-29 11:14:59 -0300

    refactor: add retorno no dto da lista de servicos da unidade de saude

--- src/main/java/br/com/bitwise/bithealth/modules/unidade_saude/dto/UnidadeSaudeResponse.java
+++ src/main/java/br/com/bitwise/bithealth/modules/unidade_saude/dto/UnidadeSaudeResponse.java
@@ -1,6 +1,9 @@
 package br.com.bitwise.bithealth.modules.unidade_saude.dto;
 
+import br.com.bitwise.bithealth.modules.servicos_saude.dto.ServicosSaudeResponse;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
 
 public record UnidadeSaudeResponse(
         @JsonProperty(namespace = "token_Id")
@@ -10,6 +13,7 @@ public record UnidadeSaudeResponse(
         @JsonProperty(namespace = "horario_inicio_atendimento")
         String horarioInicioAtendimento,
         @JsonProperty(namespace = "horario_fim_atendimento")
-        String horarioFimAtendimento
+        String horarioFimAtendimento,
+        List<ServicosSaudeResponse> listServicosSaude
 ) {
 }
