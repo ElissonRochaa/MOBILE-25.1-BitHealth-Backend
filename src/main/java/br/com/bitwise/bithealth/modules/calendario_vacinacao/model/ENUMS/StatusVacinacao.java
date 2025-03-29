940b82b02edbeeb75a4102b56579503ebc57a59c
Jeova BL <jeovab115@gmail.com>
2025-03-29 17:04:21 -0300

    feat: adiciona enum StatusVacinacao para gerenciar estados de vacinação

---
+++ src/main/java/br/com/bitwise/bithealth/modules/calendario_vacinacao/model/ENUMS/StatusVacinacao.java
@@ -0,0 +1,26 @@
+package br.com.bitwise.bithealth.modules.calendario_vacinacao.model.ENUMS;
+
+public enum StatusVacinacao {
+    AGENDADO("Agendado"),
+    CANCELADO("Cancelado"),
+    REALIZADO("Realizado");
+
+    private final String tipo;
+
+    StatusVacinacao(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public static StatusVacinacao fromString(String roleName) {
+        for (StatusVacinacao tipo : StatusVacinacao.values()) {
+            if (tipo.tipo.equalsIgnoreCase(roleName)) {
+                return tipo;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant " + StatusVacinacao.class.getCanonicalName() + "." + roleName);
+    }
+}
