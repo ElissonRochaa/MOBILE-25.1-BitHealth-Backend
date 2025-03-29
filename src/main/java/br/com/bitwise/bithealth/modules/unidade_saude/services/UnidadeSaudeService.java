a0dba387a05d64cbef5d2655283077283c4e5b40
Jeova BL <jeovab115@gmail.com>
2025-03-29 12:37:18 -0300

    feat: implementa verificação de existência e tratamento de exceções na criação e recuperação de UnidadeSaude

--- src/main/java/br/com/bitwise/bithealth/modules/unidade_saude/services/UnidadeSaudeService.java
+++ src/main/java/br/com/bitwise/bithealth/modules/unidade_saude/services/UnidadeSaudeService.java
@@ -1,8 +1,11 @@
 package br.com.bitwise.bithealth.modules.unidade_saude.services;
 
 
+import br.com.bitwise.bithealth.modules.servicos_saude.repository.ServicosSaudeRepository;
 import br.com.bitwise.bithealth.modules.unidade_saude.dto.UnidadeSaudeRequest;
 import br.com.bitwise.bithealth.modules.unidade_saude.dto.UnidadeSaudeResponse;
+import br.com.bitwise.bithealth.modules.unidade_saude.exceptions.UnidadeSaudeAlreadyExistsException;
+import br.com.bitwise.bithealth.modules.unidade_saude.exceptions.UnidadeSaudeNotFoundException;
 import br.com.bitwise.bithealth.modules.unidade_saude.model.UnidadeSaude;
 import br.com.bitwise.bithealth.modules.unidade_saude.repository.UnidadeSaudeRepository;
 import br.com.bitwise.bithealth.modules.unidade_saude.services.mapper.UnidadeSaudeMapper;
@@ -19,24 +22,34 @@ import java.util.stream.Collectors;
 public class UnidadeSaudeService {
 
     private final UnidadeSaudeRepository unidadeSaudeRepository;
+    private final ServicosSaudeRepository servicosSaudeRepository;
     private final UnidadeSaudeMapper mapperUnidadeSaude;
     private final TokenService tokenService;
 
     public UnidadeSaudeResponse createUnidadeSaude(UnidadeSaudeRequest unidadeSaudeRequest) {
+        if (unidadeSaudeRepository.getByNome(unidadeSaudeRequest.nome()) != null) {
+            throw new UnidadeSaudeAlreadyExistsException("Já existe uma unidade de saúde com o nome informado");
+        }
+
         UnidadeSaude UnidadeSaude = mapperUnidadeSaude.requestToModel(unidadeSaudeRequest);
         UnidadeSaude UnidadeSaudeSaved = unidadeSaudeRepository.save(UnidadeSaude);
-        return mapperUnidadeSaude.modelToResponse(UnidadeSaudeSaved);
+        String tokenId = tokenService.generateTokenId(String.valueOf(UnidadeSaudeSaved.getId()));
+
+        return mapperUnidadeSaude.modelToResponse(UnidadeSaudeSaved, tokenId);
     }
 
     public UnidadeSaude getUnidadeSaudeById(String tokenId) {
         String id = tokenService.decodeToken(tokenId);
-        return unidadeSaudeRepository.findById(UUID.fromString(id)).orElse(null);
+        return unidadeSaudeRepository.findById(UUID.fromString(id)).orElseThrow(() -> new UnidadeSaudeNotFoundException("Unidade de saúde não encontrada"));
     }
 
     public List<UnidadeSaudeResponse> getAllUnidadeSaude() {
-        List<UnidadeSaude> UnidadeSaudes = unidadeSaudeRepository.findAll();
-        return UnidadeSaudes.stream()
-                .map(mapperUnidadeSaude::modelToResponse)
+        List<UnidadeSaude> unidadeSaude = unidadeSaudeRepository.findAll();
+        return unidadeSaude.stream()
+                .map(x -> mapperUnidadeSaude.modelToResponse(
+                        x,
+                        tokenService.generateTokenId(String.valueOf(x.getId())
+                        )))
                 .collect(Collectors.toList());
     }
 
@@ -47,6 +60,7 @@ public class UnidadeSaudeService {
 
     public Void deleteUnidadeSaude(String tokenId) {
         String id = tokenService.decodeToken(tokenId);
+        servicosSaudeRepository.deleteByUnidadeSaudeId(UUID.fromString(id));
         unidadeSaudeRepository.deleteById(UUID.fromString(id));
         return null;
     }
