33f698ed15250f044a1d73a6bc01ded51e4e8263
Jeova BL <jeovab115@gmail.com>
2025-03-29 14:23:25 -0300

    feat: adiciona enum TipoMedicamentoEnum para categorizar tipos de medicamentos

---
+++ src/main/java/br/com/bitwise/bithealth/modules/medicamentos/model/ENUMS/TipoMedicamentoEnum.java
@@ -0,0 +1,26 @@
+package br.com.bitwise.bithealth.modules.medicamentos.model.ENUMS;
+
+
+public enum TipoMedicamentoEnum {
+    ORIGINAL("original"),
+    GENERICO("Generico");
+
+    private final String tipo;
+
+    TipoMedicamentoEnum(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public static TipoMedicamentoEnum fromString(String roleName) {
+        for (TipoMedicamentoEnum tipo : TipoMedicamentoEnum.values()) {
+            if (tipo.tipo.equalsIgnoreCase(roleName)) {
+                return tipo;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant " + TipoMedicamentoEnum.class.getCanonicalName() + "." + roleName);
+    }
+}
