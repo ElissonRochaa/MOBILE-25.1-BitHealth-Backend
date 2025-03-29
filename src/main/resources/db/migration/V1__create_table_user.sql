54e65874ee781a4a174d92389536675164de62d1
Jeova BL <jeovab115@gmail.com>
2025-03-29 17:04:03 -0300

    feat: adiciona campos idade_minima e idade_maxima na tabela calendario_vacinacao

--- src/main/resources/db/migration/V1__create_table_user.sql
+++ src/main/resources/db/migration/V1__create_table_user.sql
@@ -95,12 +95,12 @@ CREATE TABLE medicamentos (
 CREATE TABLE calendario_vacinacao (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     vacina VARCHAR(150) NOT NULL,
+    idade_minima INT NOT NULL,
+    idade_maxima INT NOT NULL,
     descricao TEXT NOT NULL,
     data_inicio DATE NOT NULL,
     data_fim DATE NOT NULL,
     status VARCHAR(50) NOT NULL,
-    unidade_saude_id UUID NOT NULL,
-    FOREIGN KEY (unidade_saude_id) REFERENCES unidades_saude(id) ON DELETE CASCADE
 );
 
 CREATE TABLE noticias (
