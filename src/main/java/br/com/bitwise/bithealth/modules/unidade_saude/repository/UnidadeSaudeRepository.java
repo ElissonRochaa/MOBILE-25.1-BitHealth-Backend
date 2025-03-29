a8b4b1ab41a926c9975f8e6119e858427b71cf80
Jeova BL <jeovab115@gmail.com>
2025-03-29 10:20:59 -0300

    fix: tipo de ID de Long modificado para UUID no repositório UnidadeSaude

--- src/main/java/br/com/bitwise/bithealth/modules/unidade_saude/repository/UnidadeSaudeRepository.java
+++ src/main/java/br/com/bitwise/bithealth/modules/unidade_saude/repository/UnidadeSaudeRepository.java
@@ -4,6 +4,8 @@ import br.com.bitwise.bithealth.modules.unidade_saude.model.UnidadeSaude;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface UnidadeSaudeRepository extends JpaRepository<UnidadeSaude, Long> {
+public interface UnidadeSaudeRepository extends JpaRepository<UnidadeSaude, UUID> {
 }
