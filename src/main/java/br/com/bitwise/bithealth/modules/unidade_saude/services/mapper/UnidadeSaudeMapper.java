6eaddbd63fd4c3702c92f6819d67ddc6a1538f24
Jeova BL <jeovab115@gmail.com>
2025-03-29 11:13:41 -0300

    feat: Retorna os serviços junto com a unidade de saude

--- src/main/java/br/com/bitwise/bithealth/modules/unidade_saude/services/mapper/UnidadeSaudeMapper.java
+++ src/main/java/br/com/bitwise/bithealth/modules/unidade_saude/services/mapper/UnidadeSaudeMapper.java
@@ -1,13 +1,25 @@
 package br.com.bitwise.bithealth.modules.unidade_saude.services.mapper;
 
+import br.com.bitwise.bithealth.modules.servicos_saude.dto.ServicosSaudeResponse;
+import br.com.bitwise.bithealth.modules.servicos_saude.model.ServicosSaude;
+import br.com.bitwise.bithealth.modules.servicos_saude.repository.ServicosSaudeRepository;
 import br.com.bitwise.bithealth.modules.unidade_saude.dto.UnidadeSaudeRequest;
 import br.com.bitwise.bithealth.modules.unidade_saude.dto.UnidadeSaudeResponse;
 import br.com.bitwise.bithealth.modules.unidade_saude.model.ENUMS.TipoUnidade;
 import br.com.bitwise.bithealth.modules.unidade_saude.model.UnidadeSaude;
+import br.com.bitwise.bithealth.security.TokenService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
+@RequiredArgsConstructor
 public class UnidadeSaudeMapper {
+
+    private final ServicosSaudeRepository servicosSaudeRepository;
+    private final TokenService tokenService;
 
     public UnidadeSaude requestToModel(UnidadeSaudeRequest unidadeSaudeRequest) {
         return new UnidadeSaude(
@@ -18,13 +30,26 @@ public class UnidadeSaudeMapper {
         );
     }
 
-    public UnidadeSaudeResponse modelToResponse(UnidadeSaude unidadeSaude, String tokenId) {
+    public UnidadeSaudeResponse modelToResponse(UnidadeSaude unidadeSaude) {
+        List<ServicosSaude> servicosSaudeList = servicosSaudeRepository.findByUnidadeSaudeId(unidadeSaude.getId());
+
+        List<ServicosSaudeResponse> servicosSaudeResponseList = servicosSaudeList.stream()
+            .map(servicosSaude -> new ServicosSaudeResponse(
+                tokenService.generateTokenId(servicosSaude.getId().toString()),
+                servicosSaude.getNome(),
+                servicosSaude.getDescricao(),
+                servicosSaude.getHorarioInicio(),
+                servicosSaude.getHorarioFim()
+            ))
+            .collect(Collectors.toList());
+
         return new UnidadeSaudeResponse(
-            tokenId,
+            tokenService.generateTokenId(unidadeSaude.getId().toString()),
             unidadeSaude.getNome(),
             unidadeSaude.getTipoUnidade().toString(),
             unidadeSaude.getHorarioInicioAtendimento(),
-            unidadeSaude.getHorarioFimAtendimento()
+            unidadeSaude.getHorarioFimAtendimento(),
+            servicosSaudeResponseList
         );
     }
 }
