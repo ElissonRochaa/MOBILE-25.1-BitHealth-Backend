02a920a690dbcd64bfac32da01a23524f44c21a9
Jeova BL <jeovab115@gmail.com>
2025-03-28 23:27:51 -0300

    feat: implementado mapper do servicos de saude para dtos e dos dtos para models

---
+++ src/main/java/br/com/bitwise/bithealth/modules/servicos_saude/services/mapper/MapperServicosSaude.java
@@ -0,0 +1,39 @@
+package br.com.bitwise.bithealth.modules.servicos_saude.services.mapper;
+
+import br.com.bitwise.bithealth.modules.servicos_saude.dto.ServicosSaudeRequest;
+import br.com.bitwise.bithealth.modules.servicos_saude.dto.ServicosSaudeResponse;
+import br.com.bitwise.bithealth.modules.servicos_saude.model.ServicosSaude;
+import br.com.bitwise.bithealth.modules.unidade_saude.model.UnidadeSaude;
+import br.com.bitwise.bithealth.modules.unidade_saude.services.UnidadeSaudeService;
+import br.com.bitwise.bithealth.security.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MapperServicosSaude {
+
+    private final UnidadeSaudeService unidadeServices;
+
+    public ServicosSaude requestToModel(ServicosSaudeRequest servicosSaudeRequest) {
+        UnidadeSaude unidadeSaude  = unidadeServices.getUnidadeSaudeById(servicosSaudeRequest.unidadeSaudeId());
+
+        return new ServicosSaude(
+                servicosSaudeRequest.nome(),
+                servicosSaudeRequest.descricao(),
+                servicosSaudeRequest.horarioInicio(),
+                servicosSaudeRequest.horarioFim(),
+                unidadeSaude
+        );
+    }
+
+    public ServicosSaudeResponse modelToResponse(ServicosSaude servicosSaudeSalvo, String tokenId) {
+        return new ServicosSaudeResponse(
+                tokenId,
+                servicosSaudeSalvo.getNome(),
+                servicosSaudeSalvo.getDescricao(),
+                servicosSaudeSalvo.getHorarioInicio(),
+                servicosSaudeSalvo.getHorarioFim()
+        );
+    }
+}
