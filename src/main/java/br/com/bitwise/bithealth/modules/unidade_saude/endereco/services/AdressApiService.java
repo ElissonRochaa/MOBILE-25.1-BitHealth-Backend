a0aca3b857bf0f182bc159eb1f34d64bf15bf6f4
Jeova BL <jeovab115@gmail.com>
2025-03-31 16:16:56 -0300

    feat: criado interface para ser implementada no enderecoUnidadeServices

--- src/main/java/br/com/bitwise/bithealth/modules/unidade_saude/endereco/services/AdressApiService.java
+++ src/main/java/br/com/bitwise/bithealth/modules/unidade_saude/endereco/services/AdressApiService.java
@@ -8,42 +8,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-@Service
-public class AdressApiService {
-
-    // Chave da API
-    private static final String API_KEY = "120b0ab6378b401aade85ddaa831ba17";
-
-    public String getCoordinatesAsString(String address) {
-        try {
-            String urlString = String.format(
-                    "https://api.opencagedata.com/geocode/v1/json?q=%s&key=%s",
-                    address.replace(" ", "+"), API_KEY);
-
-            HttpURLConnection connection = (HttpURLConnection) new URL(urlString).openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
-
-            InputStreamReader reader = new InputStreamReader(connection.getInputStream());
-            JsonObject response = JsonParser.parseReader(reader).getAsJsonObject();
-
-            if ("OK".equals(response.get("status").getAsString())) {
-                JsonObject location = response.getAsJsonArray("results")
-                        .get(0).getAsJsonObject()
-                        .getAsJsonObject("geometry")
-                        .getAsJsonObject("location");
-
-                double lat = location.get("lat").getAsDouble();
-                double lng = location.get("lng").getAsDouble();
-
-                return lat + "," + lng;
-            } else {
-                System.out.println("Erro na geocodificação: " + response.get("status").getAsString());
-                return null;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+public interface AdressApiService {
+    String getCoordinatesAsString(String address);
 }
