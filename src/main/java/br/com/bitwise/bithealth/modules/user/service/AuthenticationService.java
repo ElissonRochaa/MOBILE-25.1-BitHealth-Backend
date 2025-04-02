cc7273d47089e265dc13a6c6ae46fe2b525c5aa8
PierreOF <marcos.pierre@upe.br>
2025-04-02 07:32:44 -0300

    fix: torna o autehentication service um componente com anotacoes spring

--- src/main/java/br/com/bitwise/bithealth/modules/user/service/AuthenticationService.java
+++ src/main/java/br/com/bitwise/bithealth/modules/user/service/AuthenticationService.java
@@ -2,7 +2,9 @@ package br.com.bitwise.bithealth.modules.user.service;
 
 import br.com.bitwise.bithealth.modules.user.dto.LoginRequestDTO;
 import br.com.bitwise.bithealth.modules.user.dto.LoginResponseDTO;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface AuthenticationService {
     LoginResponseDTO authenticate(LoginRequestDTO loginRequest);
 }
